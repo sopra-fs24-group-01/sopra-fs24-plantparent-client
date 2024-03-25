26bde9399dcf7735ce13c7dee463040432f8d0a4
Danielgergely <daniel.gergely@fischerspindle.com>
2024-03-25 21:18:42 +0100

    feature(store): userSlice created #27

---
+++ src/service/userService.ts
@@ -0,0 +1,39 @@
+import { User } from "../types";
+
+const baseurl = process.env.REACT_APP_USER_BASEURL;
+
+export function getAllUsers(): Promise<User[]> {
+  return fetch(baseurl + "users/")
+    .then(response => response.json())
+    .then(data => {
+      return data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export function createUser(user: User) {
+  return fetch(baseurl + "users", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => response.json())
+    .catch(error => {
+      console.log(error)
+    });
+}
+
+export function getUser(userId: string): Promise<User> {
+  return fetch(baseurl + "users/" + userId)
+    .then(response => response.json())
+    .then(data => {
+      return data
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
