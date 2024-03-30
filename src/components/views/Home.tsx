9f81ccf9424e8689a70d41c577f3c0978b3509fc
Danielgergely <daniel.gergely@uzh.ch>
2024-03-30 23:33:34 +0100

    feature(home): basic layout created #23

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
 import Header from "./Header";
+import styled from "styled-components";
+
+const StyledMainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledSideBar = styled.div`
+  width: 20vw;
+  min-width: 200px;
+  height: calc(100vh - 80px);
+  margin-top: 79px;
+  border: 1px solid black;
+`;
+
+const StyledMainContainerContainer = styled.div`
+  width: 80vw;
+  height: calc(100vh - 80px);
+  margin-top: 79px;
+  border: 1px solid red;
+`;
 
 function Home() {
 
   return (
-    <Header />
+    <>
+      <Header />
+      <StyledMainContainer>
+        <StyledSideBar />
+        <StyledMainContainerContainer />
+      </StyledMainContainer>
+    </>
   );
 }
 
