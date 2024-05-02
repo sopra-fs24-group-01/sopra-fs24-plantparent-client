9afcba24c03a6025e1e60c7ae89bc1ad1b15e5f8
Danielgergely <daniel.gergely@uzh.ch>
2024-05-02 13:44:56 +0200

    feature(plant): qr code download working #53

--- src/components/views/QRCodeComponent.tsx
+++ src/components/views/QRCodeComponent.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { getQRCodeURL } from "../../service/qrService";
 import { StyledPrimaryButton } from "./Login";
 import styled from "styled-components";
+import { saveAs } from "file-saver";
 
 const StyledQRCodeContainer = styled.div`
   display: flex;
@@ -16,7 +17,7 @@ const StyledQRCodeContainer = styled.div`
 const StyledQrCode = styled.img`
   width: 150px;
   height: 150px;
-  
+
 `;
 
 export const StyledSecondaryButton = styled(StyledPrimaryButton)`
@@ -26,21 +27,38 @@ export const StyledSecondaryButton = styled(StyledPrimaryButton)`
 `;
 
 
-export function QRCodeComponent( { plant }: {plant: PlantFull}) {
+export function QRCodeComponent({ plant }: { plant: PlantFull }) {
   const [qrCodeURL, setQRCodeURL] = useState<string>("");
   const [showButton, setShowButton] = useState<boolean>(true);
   const url = window.location.origin;
 
   function generateQRCode() {
-    const qrUrl = getQRCodeURL({ url: url + "/plant/" + plant.plantId, size: [150, 150]});
+    const qrUrl = getQRCodeURL({ url: url + "/plant/" + plant.plantId, size: [150, 150] });
     setQRCodeURL(qrUrl);
     setShowButton(false);
   }
 
+  async function downloadImage() {
+    try {
+      const filename = plant.plantName + ".png"
+      const response = await fetch(qrCodeURL);
+      const blob = await response.blob();
+      saveAs(blob, filename);
+    } catch (e) {
+      console.log("Error downloading image: ", e);
+    }
+  }
+
   return (
     <StyledQRCodeContainer>
-      {showButton && <StyledSecondaryButton disabled={false} onClick={() => generateQRCode()}>Generate QR code</StyledSecondaryButton> }
-      {qrCodeURL && <StyledQrCode src={qrCodeURL} alt="QR code" />}
+      {showButton && <StyledSecondaryButton disabled={false} onClick={() => generateQRCode()}>Generate QR
+        code</StyledSecondaryButton>}
+      {qrCodeURL &&
+        <>
+          <StyledQrCode src={qrCodeURL} alt="QR code" />
+          <StyledSecondaryButton onClick={() => downloadImage()}>Download</StyledSecondaryButton>
+        </>
+      }
     </StyledQRCodeContainer>
-  )
+  );
 }
